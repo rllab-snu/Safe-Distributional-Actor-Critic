c17c00e14bb4e465a26616db70cb6471db80ac78
dobro12 <rlaehgudhoho@gmail.com>
2023-10-19 15:00:42 +0900

    modify cpo shell script

--- locomotion/offtrc/train/cpo_cassie2.sh
+++ locomotion/offtrc/train/cpo_cassie2.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES="0" ; python main.py \
     --env_name Cassie-v0 \
     --total_steps 4000000 \
     --limit_values "0.025, 0.025, 0.4" \
-    --n_steps 5000 \
-    --n_past_steps 5000 \
+    --n_steps 10000 \
+    --n_past_steps 10000 \
     --n_update_steps 5000 \
     --len_replay_buffer 5000 \
     --wandb
