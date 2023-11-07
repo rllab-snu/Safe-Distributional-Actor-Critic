e057f3e9f2707936598d1374a5a42684812c959f
dobro12 <rlaehgudhoho@gmail.com>
2023-11-07 15:01:14 +0900

    modify main.cpp

--- locomotion/sdac/cpp_modules2/main.cpp
+++ locomotion/sdac/cpp_modules2/main.cpp
@@ -69,27 +69,6 @@ extern "C" {
             should be "n_quantiles1 <= n_quantiles2".
             interpolation: linear 
         */
-        int current_idx;
-        int total_quantiles_num = n_quantiles1 + n_quantiles2;
-        double* integ_pmf = new double[total_quantiles_num];
-        double* integ_cmf = new double[total_quantiles_num];
-        double* sorted_quantiles = new double[total_quantiles_num];
-        double curr_cum_prob, next_cum_prob, target_cum_prob, curr_quantile, next_quantile;
-
-        int idx1 = 0, idx2 = 0;
-        for(int i=0; i<total_quantiles_num; i++){
-            if(idx2 == n_quantiles2 || (idx1 != n_quantiles1 && quantiles1[idx1] < quantiles2[idx2])){
-                integ_pmf[i] = weight1/(n_quantiles1*(weight1 + weight2));
-                sorted_quantiles[i] = quantiles1[idx1];
-                idx1++;
-            }else{
-                integ_pmf[i] = weight2/(n_quantiles2*(weight1 + weight2));
-                sorted_quantiles[i] = quantiles2[idx2];
-                idx2++;
-            }
-            if(i == 0) integ_cmf[i] = integ_pmf[i];
-            else integ_cmf[i] = integ_cmf[i - 1] + integ_pmf[i];
-        }
 
         current_idx = 0;
         for(int i=0; i<total_quantiles_num; i++){
